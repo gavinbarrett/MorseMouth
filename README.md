1a9e5a20ab56c2edc65b891b7a883a3bbcbe7934
Gavin <gavinbrrtt@gmail.com>
2020-08-19 15:21:48 -0700

    Added a README.md.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Description
+This is a small C++ utility for translating between english text and morse code. The utility will convert alphanumeric characters to their corresponding morse code representation and leave non-alphanumeric characters alone.
+
+\- .... .- -. -.- ...   ..-. --- .-.   -.-. .... . -.-. -.- .. -. --.   --- ..- -   - .... .   .-. . .--. --- !
--- morse.cc
+++ morse.cc
@@ -4,7 +4,7 @@
 
 int main() {
 
-	std::string target = "HELLO there";
+	std::string target = "Thanks for checking out the repo!";
 	int targetSize = static_cast<int>(target.size());
 
 	for (int i = 0; i < targetSize; i++) {
@@ -15,9 +15,10 @@ int main() {
 			std::cout << charMap.at(target[i]-0x20) << ' ';
 		} else if (target[i] == 0x20) {
 			std::cout << "  ";
-		} else {
+		} else if (target[i] > 0x2F && target[i] < 0x3A) {
 			std::cout << charMap.at(target[i]) << ' ';
-		}
+		} else
+			std::cout << target[i] << ' ';
 	}
 
 	return 0;
